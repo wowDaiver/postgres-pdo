6537867a09210c4a71b5325db640e9b1fdea0e85
wowDaiver <daiver@inbox.ru>
2021-03-21 02:58:21 +0300

    execute

--- index.js
+++ index.js
@@ -5,41 +5,87 @@ class Pdo {
         this._type = null;
         this._table = null;
         this._where = null;
+        this._columns = null;
+        this._values = null;
     }
 
     select() {
         this._type = 'select';
-
         return this;
     }
 
     from(table) {
         this._table = table;
+        return this;
+    }
 
+    update(pairs = null) {
+        this._type = 'update';
+        if (pairs) {
+            this.set(pairs);
+        }
+        return this;
+    }
+
+    set(pairs) {
+        this._columns = [];
+        this._values = [];
+        Object.keys(pairs).forEach(key => {
+            this._columns.push(key);
+            this._values.push(pairs[key]);
+        });
+        return this;
+    }
+
+    table(table) {
+        this._table = table;
+        return this;
+    }
+
+    insert(columns = null) {
+        this._type = 'insert';
+        this.columns(columns);
+        return this;
+    }
+
+    columns(columns) {
+        this._columns = columns;
+        return this;
+    }
+
+    values(values) {
+        this._values = values;
+        return this;
+    }
+
+    into(table) {
+        this._table = table;
+        return this;
+    }
+
+    delete(table) {
+        this._type = 'delete';
+        this._table = table;
         return this;
     }
 
     where(clause, cond, value) {
         this._where = `${clause} ${cond} ${this.escape(value)}`;
-
         return this;
     }
 
     andWhere(clause, cond, value) {
         this._where = `${this._where} AND ${clause} ${cond} ${this.escape(value)}`;
-
         return this;
     }
 
     orWhere(clause, cond, value) {
         this._where = `${this._where} OR ${clause} ${cond} ${this.escape(value)}`;
-
         return this;
     }
 
     whereIn(clause, arr) {
         this._where = `${clause} IN (${arr.map(this.escape).join(',')})`;
-
         return this;
     }
 
@@ -52,9 +98,27 @@ class Pdo {
     }
 
     execute() {
+        let query;
         switch (this._type) {
             case 'select':
-                let query = `SELECT * FROM ${this._table}`;
+                query = `SELECT * FROM ${this._table}`;
+                if (this._where) {
+                    query += ` WHERE ${this._where}`;
+                }
+                return this.client.unsafe(query);
+            case 'insert':
+                query = `INSERT INTO ${this._table} (${this._columns?.join(',')}) VALUES (${this._values?.map(this.escape).join(',')})`;
+                return this.client.unsafe(query);
+            case 'update':
+                query = `UPDATE ${this._table} SET ${this._values?.map((value, i) => {
+                    return `${this._columns[i]} = ${this.escape(value)}`;
+                }).join(',')}`;
+                if (this._where) {
+                    query += ` WHERE ${this._where}`;
+                }
+                return this.client.unsafe(query);
+            case 'delete':
+                query = `DELETE FROM ${this._table}`;
                 if (this._where) {
                     query += ` WHERE ${this._where}`;
                 }
